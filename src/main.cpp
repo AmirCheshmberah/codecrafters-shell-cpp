a4c3225287182207d55d9cef45ad05be180bf26c
Amir Cheshmberah <amirm.cheshmberah@gmail.com>
2025-01-21 03:33:49 +0330

    add input parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,54 @@
 #include <iostream>
+#include <vector>
 
-int main() {
+std::vector<std::string> input_parser(std::string& input);
+
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-
   while(true)
   {
+
     std::cout << "$ ";
 
     std::string input;
     std::getline(std::cin, input);
 
-    if(input == "exit") break;
+    std::vector<std::string> parsedInput = input_parser(input);
+
+    if(parsedInput[0] == "exit")
+    {
+      return stoi(parsedInput[1]);
+    }
 
     std::cout << input << ": command not found" << std::endl;
-
   }
+
   return 0;
+}
+
+std::vector<std::string> input_parser(std::string& input)
+{
+  input += ' ';
+  std::vector<std::string> parsedInput{};
+  parsedInput.reserve(8);
+  std::string word="";
+  for(int i = 0; i < input.length(); i++)
+  {
+    if(input[i] != ' ')
+    {
+      word += input[i];
+    }
+    else
+    {
+      if (word != "")
+        parsedInput.emplace_back(word);
+      word = "";
+    }
+  }
+  return parsedInput;
 }
