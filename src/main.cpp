aff2783bb5eed8400831cd6b608727c46f283205
Amir Cheshmberah <amirm.cheshmberah@gmail.com>
2025-01-24 20:33:41 +0330

    Exec 12

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,7 @@ int main()
 
         if(std::filesystem::exists(filePath))
         {
-          filePath = "exec " + directories[i] + '/' + input;
+          filePath = directories[i] + '/' + input;
           std::system(filePath.c_str());
           isExec = true;
         }
