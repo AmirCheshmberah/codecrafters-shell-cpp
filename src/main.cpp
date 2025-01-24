e2bfba90aec353d6c0b0b572b2a401cdc0976fac
Amir Cheshmberah <amirm.cheshmberah@gmail.com>
2025-01-24 21:02:14 +0330

    Exec 20

--- src/main.cpp
+++ src/main.cpp
@@ -77,8 +77,8 @@ int main()
 
         if(std::filesystem::exists(filePath))
         {
-          std::string command = directories[i] + ' ' + input;
-          std::system(command.c_str());
+          filePath += " " + parsedInput[1];
+          std::system(filePath.c_str());
           isExec = true;
         }
       }
