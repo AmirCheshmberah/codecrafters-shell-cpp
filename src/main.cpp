e07404c1140437fd13342839fa3eda62336d95fa
Amir Cheshmberah <amirm.cheshmberah@gmail.com>
2025-01-28 10:28:55 +0330

    Executing a quoted executable 21

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ int main()
         {
           for(int i = 0; i < parsedPathValue.size(); i++)
           {
-            std::string quoted_executable = parsedPathValue[i] + parsedInput[parsedInput.size()-1];
+            std::string quoted_executable = parsedPathValue[i] + '/' + parsedInput[parsedInput.size()-1];
             if(std::filesystem::exists(parsedInput[parsedInput.size()-1]))
             {
               std::system(quoted_executable.c_str());
