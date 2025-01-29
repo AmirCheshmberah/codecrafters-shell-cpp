93c77440ef2f35419d8c69ebe00c6bfcfed61c0e
Amir Cheshmberah <amirm.cheshmberah@gmail.com>
2025-01-29 07:10:31 +0330

    Redirect stdout 1

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <string>
 #include <filesystem>
+#include <fstream>
 
 enum ValidCommands
 {
@@ -10,6 +11,7 @@ enum ValidCommands
   echo,
   type,
   cat,
+  ls,
   exe_file
 };
 
@@ -19,14 +21,24 @@ ValidCommands isValid(std::string command)
   else if(command == "echo") return ValidCommands::echo;
   else if(command == "type") return ValidCommands::type;
   else if(command == "cat") return ValidCommands::cat;
+  else if(command == "ls") return ValidCommands::ls;
   else return ValidCommands::exe_file;
 }
 
 std::vector<std::string> mySpliter(const std::string& input, const char& delim);
 std::vector<std::string> echoParser (const std::string& input);
+std::vector<std::string> fileNamesInDirectory(const std::string& directory);
+std::vector<std::string> fileNamesInDirectory(const std::filesystem::path& directory);
 std::string doEcho(const std::string& input);
+std::string getPath(const std::string& path);
 bool isContain(const std::string& longString, const std::string& shortString);
+void handle_ls();
+void handle_cat();
+
 std::string builtin[3] = {"exit", "echo", "type"};
+
+std::vector<std::string> parsedInput;
+std::vector<std::string> parsedPathValues;
 
 int main()
 {
@@ -40,8 +52,8 @@ int main()
     std::string input;
     std::getline(std::cin, input);
 
-    std::vector<std::string> parsedInput = mySpliter(input, ' ');
-    std::vector<std::string> parsedPathValue = mySpliter(getenv("PATH"), ':');
+    parsedInput = mySpliter(input, ' ');
+    parsedPathValues = mySpliter(getenv("PATH"), ':');
 
     switch (isValid(parsedInput[0]))
     {
@@ -53,7 +65,11 @@ int main()
 
       case echo:
       {
-        std::cout << doEcho(input) << std::endl;
+        std::string _doEcho = doEcho(input);
+        if(_doEcho != "")
+        {
+          std::cout << _doEcho << std::endl;
+        }
         break;
       }
 
@@ -72,24 +88,28 @@ int main()
         }
         if(!isBuiltIn) // PATH
         {
-          for(int i = 0; i < parsedPathValue.size(); i++)
+          std::string filePath = getPath(parsedInput[1]);
+          if(filePath != "")
           {
-            std::string filePath = parsedPathValue[i] + '/' + parsedInput[1];
-            if(std::filesystem::exists(filePath))
-            {
-              std::cout << parsedInput[1] << " is " << filePath << '\n';
-              isInPath = true;
-              break;
-            }
+            std::cout << parsedInput[1] << " is " << filePath << '\n';
+            isInPath = true;
+            break;
           }
         }
         if(!isBuiltIn && !isInPath)
           std::cout << parsedInput[1] << ": not found" << '\n';
         break;
-        }
+      }
+
+      case ls:
+      {
+        handle_ls();
+        break;
+      }
+
       case cat:
       {
-        std::system(input.c_str());
+        handle_cat();
         break;
       }
       case exe_file:
@@ -103,15 +123,12 @@ int main()
             isExe = true;
           }
         }
-        for(int i = 0; i < parsedPathValue.size(); i++) // PATH
+        if(getPath(parsedInput[0]) != "")
         {
-          std::string filePath = parsedPathValue[i] + '/' + parsedInput[0];
-          if(std::filesystem::exists(filePath))
-          {
-            std::system(input.c_str());
-            isExe = true;
-          }
+          std::system(input.c_str());
+          isExe = true;
         }
+        
         if(!isExe)
           std::cout << input << ": command not found" << std::endl;
         break;
@@ -125,6 +142,61 @@ int main()
   }
 
   return 0;
+}
+
+void handle_cat()
+{
+  if(parsedInput[0] == "cat")
+  {
+    for(int i = 1; i < parsedInput.size(); i++)
+    {
+      if(std::filesystem::exists(parsedInput[i]))
+      {
+        std::string content{};
+        std::fstream f {parsedInput[i], std::ios::in};
+        while(getline(f, content))
+        {
+          std::cout << content << '\n';
+        }
+      }
+      else
+      {
+        std::cout << "cat: " << parsedInput[i] << ": No such file or directory" << std::endl;
+      }
+    }
+  }
+}
+
+void handle_ls()
+{
+  if(parsedInput[0] == "ls") // e.g "ls"
+  {
+    if(parsedInput.size() == 1)
+    {
+      for (auto& fileName : fileNamesInDirectory(std::filesystem::current_path()))
+        std::cout << fileName << ' ';
+      std::cout << '\n';
+    }
+
+    else if(parsedInput.size() == 3 && parsedInput[1] == ">") // e.g "ls > {file}"
+    {
+      std::fstream file {parsedInput[2], std::ios::out};
+      for(auto& fileName : fileNamesInDirectory(std::filesystem::current_path()))
+        file << fileName << '\n';
+    }
+
+    else if(parsedInput.size() == 4 && std::filesystem::exists(parsedInput[1]) && parsedInput[2] == ">") // e.g "ls {directory} > {file}"
+    {
+      int i{};
+      while(parsedInput[1][i] == '/')
+        i++;
+      parsedInput[1] = parsedInput[1].substr(i, parsedInput[1].length());
+
+      std::fstream file {parsedInput[3], std::ios::out};
+      for (auto& fileName : fileNamesInDirectory(parsedInput[1]))
+        file << fileName << '\n';
+    }
+  }
 }
 
 std::string doEcho(const std::string& input)
@@ -176,6 +248,25 @@ std::string doEcho(const std::string& input)
       }
       if(result != "")
         result += ' ';
+    }
+
+    if(input[i] == '>')
+    {
+      if(result.back() == ' ')
+      {
+        result = result.substr(0, result.length()-1);
+      }
+      std::string fileName{};
+      while(++i < input.length())
+      {
+        if(input[i] != ' ')
+        {
+          fileName += input[i];
+        }
+      }
+      std::fstream file {fileName, std::ios::out};
+      file << result << std::endl;
+      return "";
     }
   }
   return result;
@@ -277,4 +368,51 @@ std::vector<std::string> mySpliter(const std::string& input, const char& delim)
     }
   }
   return parsedInput;
+}
+
+std::string getPath(const std::string& pathFile)
+{
+  std::stringstream streamPathValue {getenv("PATH")};
+  std::string pathValue{};
+  while(getline(streamPathValue, pathValue, ':'))
+  {
+    std::string result = pathValue + '/' + pathFile;
+    if(std::filesystem::exists(result))
+    {
+      return result;
+    }
+  }
+  return "";
+  // std::string result;
+  // for(int i = 0; i < parsedPathValues.size(); i++)
+  // {
+  //   result = parsedPathValues[i] + '/' + path;
+  //   if(std::filesystem::exists(result))
+  //   {
+  //     return result;
+  //   }
+  // }
+  // return "";
+}
+
+std::vector<std::string> fileNamesInDirectory(const std::string& directory)
+{
+  std::vector<std::string> fileNames;
+  fileNames.reserve(8);
+  for(auto &entry : std::filesystem::directory_iterator(directory))
+  {
+    fileNames.emplace_back(entry.path().filename().generic_string());
+  }
+  return fileNames;
+}
+
+std::vector<std::string> fileNamesInDirectory(const std::filesystem::path& directory)
+{
+  std::vector<std::string> fileNames;
+  fileNames.reserve(8);
+  for(auto &entry : std::filesystem::directory_iterator(directory))
+  {
+    fileNames.emplace_back(entry.path().filename().generic_string());
+  }
+  return fileNames;
 }
